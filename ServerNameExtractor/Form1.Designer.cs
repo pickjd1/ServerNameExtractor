2d65c626d80548b85ea7ae0df1c21ee0a0c39afc
pickjd1 <pickjd1@gmail.com>
2015-12-11 10:10:56 +1000

    First Commit for ESA File converter

---
+++ ServerNameExtractor/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ServerNameExtractor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.tBxFilePath = new System.Windows.Forms.TextBox();
+            this.btnFilePath = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.savedLocationInstruction = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(298, 128);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(212, 13);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Select a File to Extract Server Names From:";
+            // 
+            // tBxFilePath
+            // 
+            this.tBxFilePath.Location = new System.Drawing.Point(159, 144);
+            this.tBxFilePath.Name = "tBxFilePath";
+            this.tBxFilePath.Size = new System.Drawing.Size(474, 20);
+            this.tBxFilePath.TabIndex = 1;
+            // 
+            // btnFilePath
+            // 
+            this.btnFilePath.Location = new System.Drawing.Point(368, 170);
+            this.btnFilePath.Name = "btnFilePath";
+            this.btnFilePath.Size = new System.Drawing.Size(75, 23);
+            this.btnFilePath.TabIndex = 2;
+            this.btnFilePath.Text = "Browse";
+            this.btnFilePath.UseVisualStyleBackColor = true;
+            this.btnFilePath.Click += new System.EventHandler(this.btnFilePath_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 262);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(474, 331);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output File Preview:";
+            // 
+            // savedLocationInstruction
+            // 
+            this.savedLocationInstruction.AutoSize = true;
+            this.savedLocationInstruction.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.savedLocationInstruction.Location = new System.Drawing.Point(275, 596);
+            this.savedLocationInstruction.Name = "savedLocationInstruction";
+            this.savedLocationInstruction.Size = new System.Drawing.Size(270, 13);
+            this.savedLocationInstruction.TabIndex = 5;
+            this.savedLocationInstruction.Text = "Files are saved to the same location as the imported file.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(41, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(744, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please export server lists via the appropriate ESA page. Then use this tool too c" +
+    "onvert the exported file too a Remote Desktop Connection Manager text file. ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(239, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "The servers will still need to be sorted into groups within RDCM.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 657);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.savedLocationInstruction);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFilePath);
+            this.Controls.Add(this.tBxFilePath);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "MainForm";
+            this.Text = "ESA File Export - Server Name Extraction";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TextBox tBxFilePath;
+        private System.Windows.Forms.Button btnFilePath;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label savedLocationInstruction;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ServerNameExtractor/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerNameExtractor
+{
+    //public static Process Start(string fileName, string arguments)
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFilePath_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            System.Windows.Forms.OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFile.FileName;
+            }
+
+            tBxFilePath.Text = fileName;
+            //|| (fileName.Contains(""))
+            if (fileName.Contains(".csv"))
+            {
+                var reader = new StreamReader(File.OpenRead(fileName));
+                List<string> serverNames = new List<string>();
+
+                var count = 0;
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Replace("\"", "").Split(',');
+
+                    if (count != 0)
+                    {
+                        if (values[0].Length != 0)
+                        {
+                            serverNames.Add(values[0]);
+                        }
+                    }
+                    count++;
+                }
+
+                TextWriter tw = new StreamWriter(fileName + "ServerNames.txt");
+                StringBuilder sb = new StringBuilder();
+
+                foreach(string s in serverNames)
+                {
+                    tw.WriteLine(s);
+                    sb.AppendLine(s);
+                }
+                textBox1.Text = sb.ToString();
+                tw.Close();
+            }
+            else
+            {
+                MessageBox.Show("Unable to process this file!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
